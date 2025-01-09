23edbdb3e731ffa12163bd36d7c5b327992c024e
Docarchi <gaspard.fontanille@laposte.net>
2025-01-09 20:51:56 +0100

    recherche bar

--- src/components/gridcreation/recherche.jsx
+++ src/components/gridcreation/recherche.jsx
@@ -1,12 +1,19 @@
-export function Recherche() {
-    return <div className="field has-addons">
+/**
+ * 
+ * @param {*} onChange 
+ * @returns 
+ */
+
+export function Recherche({onChange, inputSearch}) {
+
+    return <div className="field">
         <div className="control is-expanded">
-            <input className="input" type="text" placeholder="Entrer un mot clé..."/>
-        </div>
-        <div className="control">
-            <button className="button is-info">
-            Rechercher
-            </button>
+            <input 
+                className="input is-link" 
+                type="text"
+                value = {inputSearch}
+                placeholder="Entrer un mot clé..."
+                onChange={(e) => onChange(e.target.value)}/>
         </div>
     </div>
 }
